8cd0e4c5e7e1c484b3f886199cce33524c17ddac
Tulebaeva Marzhan <241398@astanait.edu.kz>
2024-12-18 14:11:21 +0600

    add logic

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,12 @@
+import OOP.Doctor;
+
 public class Main
 {
     public static void main(String[] args)
     {
-
+        Doctor doctor = new Doctor("John", 28, "Male");
+        System.out.println(doctor.getName());
+        doctor.setName("Egor");
+        System.out.println(doctor.getName());
     }
 }
--- src/OOP/Doctor.java
+++ src/OOP/Doctor.java
@@ -5,5 +5,29 @@ public class Doctor
     /// default constructor
     public Doctor(){}
 
+    public Doctor(String name, int age, String gender)
+    {
+        this.name = name;
+        this.age = age;
+        this.gender = gender;
+    }
 
+    private String name;
+    private int age;
+    private String gender;
+
+    public String getName()
+    {
+        return this.name;
+    }
+
+    public int getAge()
+    {
+        return this.age;
+    }
+
+    public void setName(String name)
+    {
+        this.name = name;
+    }
 }
